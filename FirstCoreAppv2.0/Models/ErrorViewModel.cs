d90863c135036508545b1fa2a7354487b22d65c7
magava <magaava@gmail.com>
2019-03-28 01:28:31 +0400

    Add project files.

---
+++ FirstCoreAppv2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstCoreAppv2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
